686a76286b54b89e18d7b966804ed89dd484f5b2
Ross <ross.srk@gmail.com>
2021-02-23 12:42:15 -0800

    Implements Redtail API lookup by phone number functionality

--- main.js
+++ main.js
@@ -21,9 +21,40 @@ function createWindow () {
   const keytar = require('keytar')
   //keytar.setPassword('zac-screen-pop', 'redtail-userkey', 'secret');
   const secret = keytar.getPassword('zac-screen-pop', 'redtail-userkey')
-  secret.then((result) => {
-    console.log("secret: "+ result); // result will be 'secret'
+  secret.then((s) => {
+    //LookupRedtailContact(s, '+1-555-456-7890')
+    LookupRedtailContact(s, '5554567890')
   });
+
+
+}
+
+function LookupRedtailContact(userkey, phoneNumber) {
+// Prepare HTTP request to Redtail CRM API
+const { net } = require('electron')
+const request = net.request({
+  method: 'GET',
+  protocol: 'https:',
+  hostname: 'smf.crm3.redtailtechnology.com',
+  port: 443,
+  path: '/api/public/v1/contacts/search?phone_number=' + phoneNumber 
+})
+request.setHeader("Authorization", userkey)
+request.setHeader("include", "addresses,phones,emails,urls")
+request.setHeader("Content-Type", "application/json")
+
+// Process HTTP response from Redtail CRM API
+request.on('response', (response) => {
+  //console.log(`STATUS: ${response.statusCode}`)
+  //console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
+  response.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`)
+  })
+  response.on('end', () => {
+    console.log('No more data in response.')
+  })
+})
+request.end()
 }
 
 app.whenReady().then(createWindow)
