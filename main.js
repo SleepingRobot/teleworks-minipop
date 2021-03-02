688aaf05e81b32cfa3d9581924014f465fd8b07a
Ross <ross.srk@gmail.com>
2021-03-01 17:00:55 -0800

    More WIP restructure of application flow

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const isDev = require('electron-is-dev');
 const keytar = require('keytar')
 const isPrimaryInstance = app.requestSingleInstanceLock()
+const keytarService = 'teleworks-screenpop'
 let screenpopWindow = null
 let historyWindow = null
 let settingsWindow = null
@@ -10,10 +11,9 @@ let contactData = []
 let redtailSettings = {
   auth: {
     valid: false,
-    user: '',
-    userKey: ''
+    name: '',
+    id: '',
   }
-  
 }
 
 if(isPrimaryInstance) {
@@ -41,11 +41,8 @@ app.whenReady().then(() => {
   parseCommandLineArgs()
 })
 
-
 function initWindows() {
-  screenpopWindow = new BrowserWindow({
-    width: 400,
-    height: 200,
+  const windowOptions = {
     webPreferences: {
       allowRunningInsecureContent: false,
       contextIsolation: true,
@@ -54,10 +51,25 @@ function initWindows() {
       sandbox: true,
       preload: `${__dirname}/preload.js`
     }
-  })
+  }
+  screenpopWindow = new BrowserWindow({...windowOptions, width: 400, height: 200})
   screenpopWindow.removeMenu()
   screenpopWindow.loadFile('screenpop.html')
   //screenpopWindow.webContents.openDevTools()
+  historyWindow = new BrowserWindow({...windowOptions, width: 400, height: 1200, show: false, parent: screenpopWindow})
+  historyWindow.removeMenu()
+  historyWindow.loadFile('history.html')
+  historyWindow.hide()
+  //historyWindow.webContents.openDevTools()
+  settingsWindow = new BrowserWindow({...windowOptions, width: 800, height: 800, show: false, parent: screenpopWindow})
+  settingsWindow.removeMenu()
+  settingsWindow.loadFile('settings.html')
+  settingsWindow.hide()
+  //settingsWindow.webContents.openDevTools()
+  authWindow = new BrowserWindow({...windowOptions, width: 400, height: 200, show: false, parent: screenpopWindow, modal: true})
+  authWindow.removeMenu()
+  authWindow.loadFile('auth.html')
+  //authWindow.webContents.openDevTools()
 }
 
 
@@ -92,7 +104,7 @@ function getCommandLineValue(argv, name) {
 function lookupRedtailContact(cliNumber) {
   // If missing valid Redtail auth, display credential input modal
   if(!redtailSettings.auth.valid) {
-    // TODO: present redtail auth modal (passing cliNumber, so it can be passed back)
+    openAuthModal('Redtail', cliNumber)
     return
   }
 
@@ -115,12 +127,15 @@ function lookupRedtailContact(cliNumber) {
   // Process HTTP response from Redtail CRM API
   // TODO: add error handling
   request.on('response', (response) => {
+    console.log(`response: ${response}`)
     response.on('data', (d) => {
       const resp = JSON.parse(d)
       // TODO: handle multiple matches. i.e., contactData[c:[{},{}], c:[{}], c:[{},{},{}], etc]
       if (resp?.contacts?.length > 0) {
         resp.contacts[0].cli_number = cliNumber
         resp.contacts[0].call_received = Date.now()
+        console.log("pushing...")
+        console.log(resp.contacts[0])
         pushContactData(resp.contacts[0])
       }
     })
@@ -128,7 +143,12 @@ function lookupRedtailContact(cliNumber) {
   request.end()
 }
 
-// Add new contact data to stack, update ScreenPop and CallHistory windows
+// Strips '+1' and all non-digit characters from number, if present
+function parseNumber (n) {
+  return n.replace('+1','').replace(/\D/g,'')
+}
+
+// Add new contact lookup results to stack, update ScreenPop and CallHistory windows
 function pushContactData(c) {
   contactData.push(c)
   // TODO: securely write new contactData array to encrypted file
@@ -141,150 +161,144 @@ function pushContactData(c) {
 
   if(historyWindow) {
     historyWindow.webContents.on('did-finish-load', ()=>{
-      historyWindow.webContents.send('history-data', c)
+      historyWindow.webContents.send('history-data', contactData)
     })
   }
 }
 
-ipcMain.on('info-request', (event) => {
-  event.sender.send('info-reply', redtailUser)
+function openAuthModal(crm, cliNumber, message = null) {
+  if (!message) {
+    message = `Enter ${crm} Account Credentials.`
+  }
+  
+  const authData = {crm: crm, cliNumber: cliNumber, message: message }
+
+  if (authWindow) {
+    authWindow.webContents.on('did-finish-load', ()=>{
+      authWindow.webContents.send('auth-data', authData)
+    })
+    authWindow.once('ready-to-show', () => {
+      authWindow.show()
+    })
+  }
+}
+
+ipcMain.on('auth-submission', (event, authData) => {
+  // Clear old auth settings, then validate new auth credentials
+  if (authData?.crm === 'Redtail') {
+    redtailSettings.auth.valid = false
+    redtailSettings.auth.name = ''
+    redtailSettings.auth.id = ''
+    keytar.deletePassword(keytarService, 'redtail-userkey').then(()=>{
+      authenticateRedtail(authData)
+    })
+  }
 })
 
-ipcMain.on('screenpop-request', (event) => {
-  event.sender.send('screenpop-reply', contactData);
-  contactData = ''
-})
+function authenticateRedtail(authData) {
+  const unencodedAuth = authData?.apiKey + ":" + authData?.username + ":" + authData?.password
+  const basicAuth = Buffer.from(unencodedAuth).toString('base64')
 
-ipcMain.on('redtail-auth-message-request', (event) => {
-  event.sender.send('redtail-auth-message-reply', redtailAuthMessage);
-  redtailAuthMessage = ''
-})
+  // Prepare HTTP request to Redtail CRM API
+  // TODO: Update this to Redtail TWAPI API if they ever start returning full user details...
+  const { net } = require('electron')
+  const request = net.request({
+    method: 'GET',
+    protocol: 'https:',
+    hostname: 'api2.redtailtechnology.com',
+    port: 443,
+    path: '/crm/v1/rest/authentication'
+  })
+  request.setHeader('Authorization', 'Basic ' + basicAuth)
+  request.setHeader('Content-Type', 'application/json')
 
-ipcMain.on('redtail-auth-submission', (event, input) => {
-  redtailUser = ''
-  redtailUserKey = ''
-  getRedtailUserKey(input.apiKey, input.username, input.password)
-  //event.sender.getOwnerBrowserWindow().close()
-})
 
+  // Process HTTP response from Redtail CRM API
+  request.on('response', (response) => {
+    if (response?.statusCode == 200) {
+      response.on('data', (d) => {
+        const resp = JSON.parse(d)
+        const userKey = JSON.parse(d).authenticated_user?.user_key
+        if (resp?.APIKey && resp?.UserKey) {
+          // If response indicates success, update Redtail auth settings
+          redtailSettings.auth.valid = true
+          redtailSettings.auth.name = resp?.Name
+          redtailSettings.auth.id = resp?.UserID
+          // then store UserKey in OS User's keychain
+          const unencodedKey = resp?.APIKey + ":" + resp?.UserKey
+          const encodedUserKey = Buffer.from(unencodedKey).toString('base64')
+          keytar.setPassword(keytarService, 'redtail-userkey', encodedUserKey)
+          // finally, if we were authenticating to fulfill a CLI number lookup, we can now re-attempt the lookup
+          if(authData?.cliNumber){
+            // Note: keytar.setPassword yields nothing, so we manually delay a couple seconds to give the OS time to store the secret first
+            setTimeout(() => {lookupRedtailContact(authData.cliNumber)}, 2000)  
+          }
+        }
+      })
+    } else if(response?.statusCode >= 400 && response?.statusCode < 500) {
+      openAuthModal('Redtail', resp?.cliNumber, `Stored Redtail authentication rejected by Redtail API as invalid (HTTP ERR ${response.statusCode.toString()}). Please re-enter credentials to try again.`)
+    } else {
+      openAuthModal('Redtail', resp?.cliNumber, `Error validating with Redtail API (HTTP ERR ${response.statusCode.toString()}). Please re-enter credentials to try again.`)
+    }
+  })
+  request.end()
+}
+
+// TODO: change this to minimize to task bar on screenpopWindow close
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-function validateRedtailUserKey() {
-  keytar.getPassword('zac-screen-pop', 'redtail-userkey').then((key) =>{
-    // If key exists in OS User's keychain, test it against Redtail CRM API
-    if (key) {
-      // Prepare HTTP request to Redtail CRM API
-      const { net } = require('electron')
-      const request = net.request({
-        method: 'GET',
-        protocol: 'https:',
-        hostname: 'api2.redtailtechnology.com',
-        port: 443,
-        path: '/crm/v1/rest/authentication'
-      })
-      request.setHeader('Authorization', 'Userkeyauth ' + key)
-      request.setHeader('Content-Type', 'application/json')
+// function validateRedtailUserKey() {
+//   keytar.getPassword('zac-screen-pop', 'redtail-userkey').then((key) =>{
+//     // If key exists in OS User's keychain, test it against Redtail CRM API
+//     if (key) {
+//       // Prepare HTTP request to Redtail CRM API
+//       const { net } = require('electron')
+//       const request = net.request({
+//         method: 'GET',
+//         protocol: 'https:',
+//         hostname: 'api2.redtailtechnology.com',
+//         port: 443,
+//         path: '/crm/v1/rest/authentication'
+//       })
+//       request.setHeader('Authorization', 'Userkeyauth ' + key)
+//       request.setHeader('Content-Type', 'application/json')
 
-      // Process HTTP response from Redtail CRM API
-      request.on('response', (response) => {
-        if (response.statusCode == 200) {
-          // If valid, save user information, then render window
-          redtailUserKey = key
-          response.on('data', (d) => {
-            const resp = JSON.parse(d)
-            const name = resp?.Name || '<Missing Name>'
-            const id = resp?.UserID || '<Missing ID>'
-            redtailUser = name + '(ID:' + id + ')'
-            displayWindow()
-          })
-        } else if(response.statusCode == 401) {
-          redtailAuthMessage = 'Stored Redtail authentication rejected by Redtail API as invalid (HTTP ERR 401). Please re-enter credentials to try again.'
-          renderHTML(400, 300, 'auth.html')
-        } else {
-          redtailAuthMessage = 'Error validating stored Redtail authentication with Redtail API (HTTP ERR' + response.statusCode.toString() + '). Please re-enter credentials to try again.'
-          renderHTML(400, 300, 'auth.html')
-        }
-      })
-      request.end()
-    } else {
-      redtailAuthMessage = 'Redtail authentication required.'
-      renderHTML(400, 300, 'auth.html')
-    }
-  })
-}
+//       // Process HTTP response from Redtail CRM API
+//       request.on('response', (response) => {
+//         if (response.statusCode == 200) {
+//           // If valid, save user information, then render window
 
-function displayWindow() {
-  if(!redtailUser || !redtailUserKey){
-    // Redtail user must be validated before proceeding
-    validateRedtailUserKey()
-  } else if (redtailLookupNumber ) {
-    // ... otherwise, if passed a Redtail phone number, query Redtail's API
-    // for matching contact information, then display screen pop
-    lookupRedtailContact(redtailLookupNumber, () => {renderHTML(300, 175, 'screenpop.html')})
-  } else {
-    // ... otherwise, if valid account but no valid parameter passed, display Info window
-    renderInfoWindow()
-  }
-}
-
-function parseNumber (n) {
-  // Removes leading +1 if present and all non-digit characters
-  return n.replace('+1','').replace(/\D/g,'')
-}
-
-function renderInfoWindow() {
-  renderHTML(400, 400, 'info.html')
-}
-
-// function renderHTML(width, height, file) {
-//   screenpopWindow.setSize(width,height)
-//   screenpopWindow.loadFile(file)
-//   // Uncomment to force open Dev Tools after loading HTML file
-//   //mainWindow.webContents.openDevTools()
-// }
-
-
-
-// function getRedtailUserKey(apiKey, username, password) {
-//   const unencodedAuth = apiKey + ":" + username + ":" + password
-//   const basicAuth = Buffer.from(unencodedAuth).toString('base64')
-
-//   // Prepare HTTP request to Redtail CRM API
-//   const { net } = require('electron')
-//   const request = net.request({
-//     method: 'GET',
-//     protocol: 'https:',
-//     hostname: 'smf.crm3.redtailtechnology.com',
-//     port: 443,
-//     path: '/api/public/v1/authentication'
-//   })
-//   request.setHeader('Authorization', 'Basic ' + basicAuth)
-//   request.setHeader('Content-Type', "application/json")
-
-//   // Process HTTP response from Redtail CRM API
-//   request.on('response', (response) => {
-//     if (response.statusCode == 200) {
-//       response.on('data', (d) => {
-//         const userKey = JSON.parse(d).authenticated_user?.user_key
-//         if (userKey) {
-//           const unencodedKey = apiKey + ":" + userKey
-//           const encodedUserKey = Buffer.from(unencodedKey).toString('base64')
-//           // If response indicates success, store UserKey in OS User's keychain
-//           keytar.setPassword('zac-screen-pop', 'redtail-userkey', encodedUserKey)
-//           // setPassword yields nothing, so we manually delay a couple seconds
-//           // to give the OS time to store the secret before any code attempts to
-//           // read it again
-//           setTimeout(displayWindow, 2000);
+//         } else if(response.statusCode == 401) {
+//           redtailAuthMessage = 'Stored Redtail authentication rejected by Redtail API as invalid (HTTP ERR 401). Please re-enter credentials to try again.'
+//           renderHTML(400, 300, 'auth.html')
+//         } else {
+//           redtailAuthMessage = 'Error validating stored Redtail authentication with Redtail API (HTTP ERR' + response.statusCode.toString() + '). Please re-enter credentials to try again.'
+//           renderHTML(400, 300, 'auth.html')
 //         }
 //       })
+//       request.end()
 //     } else {
-//       redtailAuthMessage = 'Provided Redtail credentials rejected by Redtail API (HTTP ERR ' + response.statusCode.toString() + '). Please re-enter credentials to try again.'
+//       redtailAuthMessage = 'Redtail authentication required.'
 //       renderHTML(400, 300, 'auth.html')
 //     }
 //   })
-//   request.end()
 // }
+
+// function displayWindow() {
+//   if(!redtailUser || !redtailUserKey){
+//     // Redtail user must be validated before proceeding
+//     validateRedtailUserKey()
+//   } else if (redtailLookupNumber ) {
+//     // ... otherwise, if passed a Redtail phone number, query Redtail's API
+//     // for matching contact information, then display screen pop
+//     lookupRedtailContact(redtailLookupNumber, () => {renderHTML(300, 175, 'screenpop.html')})
+//   } else {
+//     // ... otherwise, if valid account but no valid parameter passed, display Info window
+//     renderInfoWindow()
+//   }
+// }
+
